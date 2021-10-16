ed51a30a3247b7c56a45baf0174b95cfa6a9d2db
Gabriel Lúcio <bielmengao_@hotmail.com>
2021-10-16 11:32:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMultifuncional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppMultifuncional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
